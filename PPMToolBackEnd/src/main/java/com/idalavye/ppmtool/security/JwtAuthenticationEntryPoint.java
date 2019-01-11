1e70d93ed6b6d3e23be56f1df675df7d03977986
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-01-11 23:57:59 +0300

    edited

--- PPMToolBackEnd/src/main/java/com/idalavye/ppmtool/security/JwtAuthenticationEntryPoint.java
+++ PPMToolBackEnd/src/main/java/com/idalavye/ppmtool/security/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,27 @@
 package com.idalavye.ppmtool.security;
 
-public class JwtAuthenticationEntryPoint {
+import com.google.gson.Gson;
+import com.idalavye.ppmtool.exceptions.InvalidLoginResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        //kullanıcı giriş yapmaya çalıştığında bir hata ile karşılaştığı zman tetiklenecek
+        InvalidLoginResponse loginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(loginResponse);
+
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setStatus(401);
+        httpServletResponse.getWriter().print(jsonLoginResponse);
+    }
 }
